21b6ed0aa9d3e008bc8b0cd158720ab434bb2630
manceret <manceret@c2s.fr>
2020-10-19 17:04:08 +0200

    Création d'un composant toggle permet de basculer entre les thèmes dark/light

---
+++ src/js/toggle.js
@@ -0,0 +1,43 @@
+﻿function toggleTheme() {
+    if (localStorage.getItem('theme') === 'dark') {
+        setTheme('light');
+    } else {
+        setTheme('dark');
+    }     
+}
+
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.setAttribute('data-theme', themeName);
+
+    document.documentElement.className = '';
+    document.documentElement.classList.add(`theme--${themeName}`);
+}
+
+(function() {
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    // Un thème a été choisi par l'utilisateur sur ce site, on l'applique
+    if (currentTheme) {
+        if (currentTheme === 'dark') {
+            setTheme('dark');
+            document.getElementById('slider').checked = false;
+        } else {
+            setTheme('light');
+            document.getElementById('slider').checked = true;
+        }
+    }
+    else {
+        // Pas encore de choix, on regarde les préférences de l'OS/navigateur
+        if (window.matchMedia("(prefers-color-scheme: dark)")){
+            setTheme('dark');
+            document.getElementById('slider').checked = false;
+        }
+        else {
+            // TODO: Thème par défaut ?
+        }
+    }
+
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => e.matches && toggleTheme() )
+    window.matchMedia("(prefers-color-scheme: light)").addListener(e => e.matches && toggleTheme() )
+})();
